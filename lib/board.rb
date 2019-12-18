3c85616eb920b70a789121dc2c4721c0dff40273
Robert <bibbycodes@gmail.com>
2019-12-18 11:42:51 +0000

    validates input to add function

--- lib/board.rb
+++ lib/board.rb
@@ -3,16 +3,24 @@
 class Board
   def initialize
     @board = {
-      "A" => { "1": '',  "2": '', "3": '' },
-      "B" => { "1": '',  "2": '', "3": '' },
-      "C" => { "1": '',  "2": '', "3": '' }
+      'A' => { "1": '',  "2": '', "3": '' },
+      'B' => { "1": '',  "2": '', "3": '' },
+      'C' => { "1": '',  "2": '', "3": '' }
     }
   end
 
   def add(piece, position)
     row = position[0]
     column = position[1].to_sym
-    @board[row][column] = piece
-    [@board["A"], @board["B"], @board["C"]]
+    if "ABC".include?(position[0]) || "123".include?(position[1])
+      if @board[row][column] == ''
+        @board[row][column] = piece
+      else
+        return "Cannot add piece to a spot that has been taken"
+      end
+    else
+      return "Cannot add piece to a spot outside the board"
+    end
+    [@board['A'], @board['B'], @board['C']]
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -14,58 +14,61 @@ describe Board do
       @board = Board.new
     end
     it 'can add an X to the board at A 1' do
-      expect(@board.add('X', "A1")).to eq([
-                                               { "1": 'X', "2": '', "3": '' },
-                                               { "1": '',  "2": '', "3": '' },
-                                               { "1": '',  "2": '', "3": '' }
-                                             ])
+      expect(@board.add('X', 'A1')).to eq([
+                                            { "1": 'X', "2": '', "3": '' },
+                                            { "1": '',  "2": '', "3": '' },
+                                            { "1": '',  "2": '', "3": '' }
+                                          ])
     end
 
     it 'can add an O to the board at A 1' do
-      expect(@board.add('O', "A1")).to eq([
-                                               { "1": 'O', "2": '', "3": '' },
-                                               { "1": '',  "2": '', "3": '' },
-                                               { "1": '',  "2": '', "3": '' }
-                                             ])
+      expect(@board.add('O', 'A1')).to eq([
+                                            { "1": 'O', "2": '', "3": '' },
+                                            { "1": '',  "2": '', "3": '' },
+                                            { "1": '',  "2": '', "3": '' }
+                                          ])
     end
 
     it 'can add an X to the board at A 2' do
-      expect(@board.add('X', "A2")).to eq([
-                                               { "1": '', "2": 'X', "3": '' },
-                                               { "1": '', "2": '',  "3": '' },
-                                               { "1": '', "2": '',  "3": '' }
-                                             ])
+      expect(@board.add('X', 'A2')).to eq([
+                                            { "1": '', "2": 'X', "3": '' },
+                                            { "1": '', "2": '',  "3": '' },
+                                            { "1": '', "2": '',  "3": '' }
+                                          ])
     end
 
     it 'can add an O to the board at B 1' do
-      expect(@board.add('O', "B1")).to eq([
-                                               { "1": '', "2": '',  "3": '' },
-                                               { "1": 'O', "2": '', "3": '' },
-                                               { "1": '',  "2": '', "3": '' }
-                                             ])
+      expect(@board.add('O', 'B1')).to eq([
+                                            { "1": '', "2": '',  "3": '' },
+                                            { "1": 'O', "2": '', "3": '' },
+                                            { "1": '',  "2": '', "3": '' }
+                                          ])
     end
 
     it 'can add an X to the board at C 2' do
-      expect(@board.add('X', "C2")).to eq([
-                                               { "1": '', "2": '',  "3": '' },
-                                               { "1": '', "2": '',  "3": '' },
-                                               { "1": '', "2": 'X', "3": '' }
-                                             ])
+      expect(@board.add('X', 'C2')).to eq([
+                                            { "1": '', "2": '',  "3": '' },
+                                            { "1": '', "2": '',  "3": '' },
+                                            { "1": '', "2": 'X', "3": '' }
+                                          ])
     end
 
     it 'can add an O to the board at C 3 and then an X at A 1' do
-      @board.add('X', "A1")
-      expect(@board.add('O', "C3")).to eq([
-                                               { "1": 'X', "2": '',  "3": '' },
-                                               { "1": '',  "2": '',  "3": '' },
-                                               { "1": '',  "2": '',  "3": 'O' }
-                                             ])
+      @board.add('X', 'A1')
+      expect(@board.add('O', 'C3')).to eq([
+                                            { "1": 'X', "2": '', "3": '' },
+                                            { "1": '',  "2": '',  "3": '' },
+                                            { "1": '',  "2": '',  "3": 'O' }
+                                          ])
     end
 
-    # it 'cannot add a piece to a spot that has been taken' do
-    #   @board.add("X", "A1")
-    #   expect(@board.add("X", "A1")).to eq("Cannot Add piece to a spot that has been taken")
-    # end
-  end
+    it 'cannot add a piece to a spot that has been taken' do
+      @board.add("X", "A1")
+      expect(@board.add("X", "A1")).to eq("Cannot add piece to a spot that has been taken")
+    end
 
+    it 'cannot add a piece to a spot outside the board' do
+      expect(@board.add("X", "D4")).to eq("Cannot add piece to a spot outside the board")
+    end
+  end
 end
