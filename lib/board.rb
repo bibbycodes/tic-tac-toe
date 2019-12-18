48872b83249d566fd80879f23c83ed9c522e71bc
Robert <bibbycodes@gmail.com>
2019-12-18 10:49:55 +0000

    can add a piece at C2

--- lib/board.rb
+++ lib/board.rb
@@ -9,6 +9,10 @@ class Board
   def add(piece, position)
     if position[0] == "A"
       @A[position[1].to_sym] = piece
+    elsif position[0] == "B"
+      @B[position[1].to_sym] = piece
+    elsif position[0] == "C"
+      @C[position[1].to_sym] = piece
     end
     @board
   end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,16 +1,19 @@
 # Acceptance Criteria:
-#   Should be a 3 x 3 board
-#   Should tell when the board is full
+#   is a 3 x 3 board
+#   can tell when the board is full
 #   Prevents players from placing a piece in the same spot more than once
+#   Players can add a piece to the board
 
 require 'board'
 
 describe Board do
-  before(:each) do
-    @board = Board.new
-  end
+
   context 'adding a piece' do
-    it 'should add an X to the board at A 1' do
+    before(:each) do
+      @board = Board.new
+    end
+    
+    it 'can add an X to the board at A 1' do
       expect(@board.add("X", ["A", "1"])).to eq([
         {"1": "X", "2": "", "3": ""}, 
         {"1": "",  "2": "", "3": ""}, 
@@ -18,7 +21,7 @@ describe Board do
       ]) 
     end
 
-    it 'should add an O to the board at A 1' do
+    it 'can add an O to the board at A 1' do
       expect(@board.add("O", ["A", "1"])).to eq([
         {"1": "O", "2": "", "3": ""}, 
         {"1": "",  "2": "", "3": ""}, 
@@ -26,11 +29,27 @@ describe Board do
       ]) 
     end
 
-    it 'should add an X to the board at A 2' do
+    it 'can add an X to the board at A 2' do
       expect(@board.add("X", ["A", "2"])).to eq([
         {"1": "", "2": "X", "3": ""}, 
         {"1": "",  "2": "", "3": ""}, 
         {"1": "",  "2": "", "3": ""}
+      ]) 
+    end
+
+    it 'can add an O to the board at B 1' do
+      expect(@board.add("O", ["B", "1"])).to eq([
+        {"1": "",  "2": "", "3": ""}, 
+        {"1": "O", "2": "", "3": ""}, 
+        {"1": "",  "2": "", "3": ""}
+      ]) 
+    end
+
+    it 'can add an X to the board at C 2' do
+      expect(@board.add("X", ["C", "2"])).to eq([
+        {"1": "", "2": "",  "3": ""}, 
+        {"1": "", "2": "",  "3": ""}, 
+        {"1": "", "2": "X", "3": ""}
       ]) 
     end
   end
