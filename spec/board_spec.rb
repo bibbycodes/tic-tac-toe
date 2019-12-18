7bdb566c1c5f2565bee3ac844030e742b4fc4752
Robert <bibbycodes@gmail.com>
2019-12-18 10:10:02 +0000

    planning and acceptance criteria

---
+++ spec/board_spec.rb
@@ -0,0 +1,4 @@
+# Acceptance Criteria:
+#   Should be a 3 x 3 board
+#   Should tell when the board is full
+#   Prevents players from placing a piece in the same spot more than once
---
+++ spec/player_spec.rb
@@ -0,0 +1,2 @@
+#   Has A Name (Defaults to Player 1 or Player 2)
+#   Has a Piece - X or O
