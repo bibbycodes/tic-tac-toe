a4156147424b9e18a634fd5dca62455e7f77cad0
Robert <bibbycodes@gmail.com>
2019-12-18 11:34:22 +0000

    refactored add function

--- lib/board.rb
+++ lib/board.rb
@@ -2,20 +2,17 @@
 
 class Board
   def initialize
-    @A = { "1": '',  "2": '', "3": '' }
-    @B = { "1": '',  "2": '', "3": '' }
-    @C = { "1": '',  "2": '', "3": '' }
-    @board = [@A, @B, @C]
+    @board = {
+      "A" => { "1": '',  "2": '', "3": '' },
+      "B" => { "1": '',  "2": '', "3": '' },
+      "C" => { "1": '',  "2": '', "3": '' }
+    }
   end
 
   def add(piece, position)
-    if position[0] == 'A'
-      @A[position[1].to_sym] = piece
-    elsif position[0] == 'B'
-      @B[position[1].to_sym] = piece
-    elsif position[0] == 'C'
-      @C[position[1].to_sym] = piece
-    end
-    @board
+    row = position[0]
+    column = position[1].to_sym
+    @board[row][column] = piece
+    [@board["A"], @board["B"], @board["C"]]
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -9,13 +9,12 @@
 require 'board'
 
 describe Board do
-  context 'adding a piece' do
+  context 'adding a piece to the board' do
     before(:each) do
       @board = Board.new
     end
-
     it 'can add an X to the board at A 1' do
-      expect(@board.add('X', %w[A 1])).to eq([
+      expect(@board.add('X', "A1")).to eq([
                                                { "1": 'X', "2": '', "3": '' },
                                                { "1": '',  "2": '', "3": '' },
                                                { "1": '',  "2": '', "3": '' }
@@ -23,7 +22,7 @@ describe Board do
     end
 
     it 'can add an O to the board at A 1' do
-      expect(@board.add('O', %w[A 1])).to eq([
+      expect(@board.add('O', "A1")).to eq([
                                                { "1": 'O', "2": '', "3": '' },
                                                { "1": '',  "2": '', "3": '' },
                                                { "1": '',  "2": '', "3": '' }
@@ -31,15 +30,15 @@ describe Board do
     end
 
     it 'can add an X to the board at A 2' do
-      expect(@board.add('X', %w[A 2])).to eq([
+      expect(@board.add('X', "A2")).to eq([
                                                { "1": '', "2": 'X', "3": '' },
-                                               { "1": '',  "2": '', "3": '' },
-                                               { "1": '',  "2": '', "3": '' }
+                                               { "1": '', "2": '',  "3": '' },
+                                               { "1": '', "2": '',  "3": '' }
                                              ])
     end
 
     it 'can add an O to the board at B 1' do
-      expect(@board.add('O', %w[B 1])).to eq([
+      expect(@board.add('O', "B1")).to eq([
                                                { "1": '', "2": '',  "3": '' },
                                                { "1": 'O', "2": '', "3": '' },
                                                { "1": '',  "2": '', "3": '' }
@@ -47,11 +46,26 @@ describe Board do
     end
 
     it 'can add an X to the board at C 2' do
-      expect(@board.add('X', %w[C 2])).to eq([
+      expect(@board.add('X', "C2")).to eq([
                                                { "1": '', "2": '',  "3": '' },
                                                { "1": '', "2": '',  "3": '' },
                                                { "1": '', "2": 'X', "3": '' }
                                              ])
     end
+
+    it 'can add an O to the board at C 3 and then an X at A 1' do
+      @board.add('X', "A1")
+      expect(@board.add('O', "C3")).to eq([
+                                               { "1": 'X', "2": '',  "3": '' },
+                                               { "1": '',  "2": '',  "3": '' },
+                                               { "1": '',  "2": '',  "3": 'O' }
+                                             ])
+    end
+
+    # it 'cannot add a piece to a spot that has been taken' do
+    #   @board.add("X", "A1")
+    #   expect(@board.add("X", "A1")).to eq("Cannot Add piece to a spot that has been taken")
+    # end
   end
+
 end
